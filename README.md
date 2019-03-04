6f2c9fcfdc833dbcf358719771f75ea7b929dd0f
luke <luke@localhost.localdomain>
2019-03-03 22:15:42 -0500

    Finalized Commit

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # flask-server-diagnostic-web-api
-placeholder
+
+A Web API for launching a simplistic diagnostic tool for your servers.
+
+Supports memory information and file system information from a server.
+Uses a 32 character long string token made of numbers and case insensitive letters to authenticate any client connecting to the server that requests for the diagnostic information.
+
+Setup
+======
+Requires flask for python web API services, and python 3.
+
+```bash
+sudo pip install flask
+```
+
+If you have pip. If not, you will need to install pip for python3.
+
+Running
+------
+Execution is straightforward:
+
+```bash
+python3 diagnosticService.py <ip-address> <port>
+```
+
+Where ip-address is the ip address that the web service should use (this allows flexibility in limiting access from outside a network or internal testing)
+Where port is the port number it should listen from (Do not forget to open said airport on local linux distributions via ufw / iptables / firewall-cmd)
+
+You could also include python's virtualenv to run it as an independent process headless, or use java framework jenkins, or any custom system of your desire.
+
+Trying it on curl:
+
+```bash
+curl -X GET 'http://127.0.0.1:9999/diagnostics?token=123123
+```
+
+where 127.0.0.1 is the ip address the program listens on, 9999 is the port that the program listen on, and the token to authenticate is 123123
+
+The output will be a JSON encoded hash dataset with memory and filesystem information found using the bash scripts 'free -m' and 'df -h'
---
+++ diagnosticService.py
@@ -0,0 +1,106 @@
+#!flask/bin/python
+
+# Simple Web API that prints diagnostics information
+# By: Luke (luke.a.programmer@gmail.com)
+# Last Updated: 3/2/2019
+# 
+# Requirements: Flask, python 3.4.9 or better.
+# Download flask using pip: sudo pip install flask
+# 
+# Authentication token is provided to ensure that malicious intent doesn't occur.
+# Currently the program provides information on memory use and disk use from commands 'free -m' and 'df -h'
+# which can be seen in the diagnostics.sh bash script (which is required in the same directory as this program to work)
+#
+# Recommend using python's virtualenv to run an instance of the service independently from your CLI
+# 
+
+from flask import Flask, jsonify, request, abort
+import subprocess, random, string, sys
+
+app = Flask(__name__)
+
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web', 
+        'done': False
+    }
+]
+
+authentication_token=''.join(random.choice(string.ascii_uppercase+string.ascii_lowercase+string.digits) for _ in range(32))
+
+
+@app.route('/diagnostics', methods=['GET'])
+def get_tasks():
+    global authentication_token
+
+    # To avoid malicious intent, check the token passed or else we return a default 404
+    if authentication_token != request.args.get('token'):
+        abort(404)
+
+    proc = subprocess.Popen(['./diagnostics.sh'], stdout=subprocess.PIPE, shell=False)
+
+    (output,errors) = proc.communicate()
+
+    output=output.decode('utf-8')
+    output=output.strip().split("\n\n")
+
+    holder=output[0].strip().split('\n')
+
+    final_output={}
+    final_output[holder[0]]={}
+
+    for j,y in enumerate(holder[1:]):
+        hold2 = y.split(":")
+        print(hold2)
+        hold2[1] = hold2[1].split('|')
+
+        tempDict={}
+
+        for z in hold2[1]:
+            z=z.split(",")
+            tempDict[z[0]]=z[1]
+
+        final_output[holder[0]][hold2[0]]=tempDict
+
+
+    holder=output[1].strip().split('\n')
+
+    final_output[holder[0]]=[]
+
+    tmp = []
+
+    for i,x in enumerate(holder[1:]):
+        x=x.split("|")
+
+        tempDict={}
+
+        for z in x:
+            z=z.split(",")
+
+            tempDict[z[0]]=z[1]
+
+
+        x=tempDict
+
+        final_output[holder[0]].append(x)
+
+    return jsonify(final_output)
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print("{} <IP Address> <Port Number>".format(sys.argv[0]))
+        sys.exit(0)
+
+    print("Authentication Token: " + authentication_token)
+    print("Use this parameter for calling diagnostics: ?token="+authentication_token)
+    print("")
+    # app.run(debug=True,host='192.168.0.30',port=9999)
+    app.run(host=sys.argv[1],port=int(sys.argv[2]))
